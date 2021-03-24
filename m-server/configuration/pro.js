0fe15877250c7548f031e7fb1b29374ca288ab36
xzhasdf <35563768+xzhasdf@users.noreply.github.com>
2021-03-24 14:59:47 +0800

    Update pro.js

--- m-server/configuration/pro.js
+++ m-server/configuration/pro.js
@@ -1,5 +1,5 @@
 module.exports = {
-    databaseAddress: 'mongodb://127.0.0.1:27017/<%=projectName%>Pro',
+    databaseAddress: 'mongodb://127.0.0.1:27017/<%=dbName%>Pro',
     baseEmail:'@<%=projectName%>.com',
     port: 3001,
     baseUrl: '/v1/<%=projectName%>/',
