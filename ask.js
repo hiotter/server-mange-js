ef11065d978def52fbabd91965336600aeaabc64
xzhasdf <35563768+xzhasdf@users.noreply.github.com>
2021-03-24 14:58:10 +0800

    Update ask.js

--- ask.js
+++ ask.js
@@ -2,7 +2,12 @@ module.exports = [
     {
         type: 'input',
         name: 'projectName',
-        message: '项目名称?',
+        message: '项目名称(默认admin)?',
+    },
+    {
+        type: 'input',
+        name: 'dbName',
+        message: '数据库名称(默认admin)?',
     },
     {
         type: 'input',
